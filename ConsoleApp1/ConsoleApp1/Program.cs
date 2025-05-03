8a98af722ae6038f9a0c1fcbaaa6dc39f33130e2
DA05vy <dda05vy@gmail.com>
2025-05-03 16:21:18 +0700

    Add files via upload
    ConsoleApp1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        switch (a)
+        {
+            case 0:
+                Console.WriteLine("case 0");
+        }
+
+        int b = 20;
+        Console.WriteLine( (b>8)? "YES": "NO");
+    }
+}
