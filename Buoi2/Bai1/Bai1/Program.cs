4029ad017aae6a7f61b4ca222916b4b3b8b1b1c5
DA05vy <dda05vy@gmail.com>
2025-05-03 16:14:03 +0700

    Add files via upload
    Bai2C#

---
+++ Buoi2/Bai1/Bai1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Bai1;
+
+class Program
+{
+    static void Main( string [] args )
+    {
+        //B1a
+        double a = 9;
+        double T = 2.0/5.0;
+        double t1 = 2.125;
+        int rT = (int)(t1 / T);
+        double dT = t1 % T;
+        double kc = rT * 4 * a + 2 * a * Math.Abs(Math.Sin(5 * Math.PI * dT));
+        Console.WriteLine($"Quang duong vat A di duoc sau {t1} s: {kc} cm" );
+        
+        //B1b
+        Console.Write("Nhap a:");
+        double aB = double.Parse(Console.ReadLine());
+        Console.Write("Nhap x:");
+        double x = double.Parse(Console.ReadLine());
+        Console.Write("Nhap t:");
+        double t = double.Parse(Console.ReadLine());
+        Console.WriteLine($"{a} cos( {x}pi + pi/2 )");
+        double TB = 2.0 / x;
+        double rTB = t / TB;
+        double dTB = t % TB;
+        double kcb = rTB * 4 * aB + 2 * aB * Math.Abs(Math.Sin(x * Math.PI * dTB));
+        Console.WriteLine($"Quang duong vat A di duoc sau {t} s: {kcb} cm" );
+    }
+}
---
+++ Buoi2/Bai2/Bai2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Bai2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double vo = 20;
+        double the = 30;
+        double g = 9.8;
+        double phi = the / 180;
+        
+        //a
+        double vox = 20 * Math.Cos(phi * Math.PI);
+        Console.WriteLine($" Van toc theo phuong ngang: {vox:F2} m/s");
+        double voy = 20*Math.Sin(phi * Math.PI);
+        Console.WriteLine($" Van toc theo phuong thang dung: {voy:F2} m/s");
+        
+        //b
+        double t = voy / g;
+        Console.WriteLine($" Thoi gian vat len diem cao nhat: {t:F2} s");
+        
+        //c
+        double h = voy * t - 0.5 * g * t * t;
+        Console.WriteLine($" Chieu cao cuc dai cua vat: {h:F2} m");
+        
+        //d
+        double d = vox * 2 * t;
+        Console.WriteLine($" Quang duong di cua vat: {d:F2} m");
+
+    }
+}
