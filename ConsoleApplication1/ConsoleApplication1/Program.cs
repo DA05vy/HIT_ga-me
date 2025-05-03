7af07bf35cde8e72259a87ea8ea58fb3c1ee0be5
DA05vy <dda05vy@gmail.com>
2025-05-03 16:21:38 +0700

    Add files via upload
    ConsoleApp2

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            object so1 = 10;
+            Console.WriteLine("");
+            Console.Write(" ");
+            int a = Convert.ToInt32(Console.ReadLine());a
+            int b = Convert.ToInt32(Console.ReadLine());
+            int H, K, T;
+            H = K = T = 10;
+            Console.WriteLine(" H = {0}, K = {1}, T = {2}", H, K, T);
+            H += K = T = 5;
+            
+        }
+    }
+    
+}
+    
