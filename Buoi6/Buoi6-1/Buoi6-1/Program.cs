1ee09a5474960f735a673208529b3989d6a5bccf
DA05vy <dda05vy@gmail.com>
2025-05-03 16:20:01 +0700

    Add files via upload
    Bai6C#

---
+++ Buoi6/Buoi6-1/Buoi6-1/Program.cs
@@ -0,0 +1,110 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+
+public enum State
+    {
+        Idle,
+        Move,
+        Attack
+    }
+namespace Buoi6_1
+{
+    
+    public class Character
+    {
+        public void Change(State state)
+        {
+            switch (state)
+            {
+                case State.Idle:
+                    Console.WriteLine("Idle");
+                    break;
+                case State.Move:
+                    Console.WriteLine("Move");
+                    break;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        //public static void Main(string[] args)
+        //{
+        //   Character player = new Character();
+        //    player.Change(State.Idle);
+        //}
+        //static void Hien<T>(T x)
+        //{
+        //    Console.WriteLine(x);
+        //}
+        //public delegate void MyDeLangte();
+        public delegate int MyDeLangte(int x,int y);
+
+        public static int Cong (int x, int y)
+        {
+            return x + y;
+        }
+        public static int Tru (int x, int y)
+        {
+            return x - y;
+        }
+
+        public static void Hien0()
+        {
+            Console.WriteLine("Hien0");
+        }
+        
+        public static void Hien1()
+        {
+            Console.WriteLine("Hien1");
+        }
+        public static void Hien2()
+        {
+            Console.WriteLine("Hien2");
+        }
+
+        public static void XXXX(Action action)
+        {
+            if (action == null)
+            {
+                return;
+            }
+
+            action();
+        }
+
+        public class Animal()
+        {
+            public virtual void In()
+            {
+                Console.WriteLine("0");
+            }
+        }
+
+        public class Dog() : Animal
+        {
+            public override void In()
+            {
+                Console.WriteLine("1");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            //MyDeLangte myDeLangte = new MyDeLangte(Hien0);
+            //MyDeLangte myDeLangte2 = new MyDeLangte(Hien1);
+            //MyDeLangte myDeLangte3 = new MyDeLangte(Hien2);
+            //Hien0();
+            //Hien1();
+            //Hien2();
+            
+            MyDeLangte del1 = new MyDeLangte(Cong);
+            MyDeLangte del2 = new MyDeLangte(Tru);
+            MyDeLangte del3 = del1 + del2;
+            Console.WriteLine(del3(2, 1));
+
+            Animal animal = new Dog();
+            animal.In();
+        }
+    }
+}
---
+++ Buoi6/Buoi6/Buoi6/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Buoi6
+{
+
+    public interface IEmployee
+    {
+        int calculateSalary();
+        string name { get; set; }
+    }
+
+    public class Employee : IEmployee
+    {
+        public string name { get; set; }
+        public int paymentPerHour { get; set; }
+
+        public Employee(string name, int paymentPerHour)
+        {
+            this.name = name;
+            this.paymentPerHour = paymentPerHour;
+        }
+
+        public int calculateSalary()
+        {
+            return this.paymentPerHour *20;
+        }
+    }
+
+
+    public class PartTimeEmployee : Employee
+    {
+        public int workingHours{ get; set; }
+
+        public PartTimeEmployee(string name, int paymentPerHour, int workingHours)
+            : base(name, paymentPerHour)
+        {
+            this.workingHours = workingHours;
+        }
+
+        public int calculateSalary()
+        {
+            return workingHours * this.paymentPerHour;
+        }
+    }
+
+    public class FullTimeEmployee : Employee
+    {
+        public FullTimeEmployee(string name, int paymentPerHour)
+            : base(name, paymentPerHour)
+        {
+        }
+
+        public int calculateSalary()
+        {
+            return paymentPerHour * 20;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create part-time and full-time employees
+            PartTimeEmployee partTime = new PartTimeEmployee("John Doe", 15, 20);
+            FullTimeEmployee fullTime = new FullTimeEmployee("Jane Smith", 20);
+
+            // Output their salaries
+            Console.WriteLine($"{partTime.name} (Part-Time): ${partTime.calculateSalary()}");
+            Console.WriteLine($"{fullTime.name} (Full-Time): ${fullTime.calculateSalary()}");
+        }
+    }
+}
