863931b6864c2539ae078db91c771dfffe17978f
DA05vy <dda05vy@gmail.com>
2025-05-03 16:16:13 +0700

    Add files via upload
    Bai4C#

---
+++ Buoi4/Bai4/Bai4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Bai4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int t = int.Parse(Console.ReadLine()); 
+        while (t-- > 0)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                string[] val = input.Split();
+                int first = int.Parse(val[0]);
+                int second = int.Parse(val[1]);
+                var pair = Tuple.Create(first, second);
+                Console.WriteLine($"Pair: ({pair.Item1}, {pair.Item2})");
+            }
+        }
+    }
+}
+    
